8276da9d97b54958d8df052f312504a4661f3eca
Mewen Batty <scorpionem86@gmail.com>
2026-09-23 15:46:03 +0200

    aabbrenderer

---
+++ src/render/renderer/AABBRenderer.cpp
@@ -0,0 +1,6 @@
+#include "render/renderer/AABBRenderer.hpp"
+
+mbl::render::Shader*	mbl::render::renderer::AABBRenderer::_ext_shader;
+mbl::render::Mesh*		mbl::render::renderer::AABBRenderer::_ext_mesh;
+mbl::render::Shader		mbl::render::renderer::AABBRenderer::_int_shader;
+mbl::render::Mesh		mbl::render::renderer::AABBRenderer::_int_mesh;
